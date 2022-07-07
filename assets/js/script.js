02e1489cfb27985af21379d258c56c439943a96c
Christy Le <christylex3@gmail.com>
2022-07-07 15:21:06 -0400

    Receiving response from Words API for part of speech

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 const storyRequest = 'http://madlibz.herokuapp.com/api/random';
-const wordRequest = 'https://wordsapiv1.p.mashape.com/words/';
+const wordRequest = 'https://wordsapiv1.p.rapidapi.com/words/';
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6305df614bmsh1e297ce718d1c45p1b597djsnf7ec4eaf952e',
+		'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+	}
+};
 
 var submitBtn = $('<button>Make Me a Story!</button>');
 var inputForm = $('<form>'); // makes a new form element
@@ -25,6 +33,16 @@ startBtn.on('click', function() { //start button event listener
     getStory(requestUrl); // sends a request to the madLibz api to grab a random story
 });
 
+inputForm.on('click', '.random', function(event) {
+    event.preventDefault();
+    console.log("Random button is clicked");
+    var wordPos = $(this).siblings('input').attr('placeholder'); // storing the part of speech
+    var randWordRequest = wordRequest + "?random=true?partofspeech=" + wordPos; // making request call based on part of speech
+    // var randWordRequest = "https://wordsapiv1.p.mashape.com/words/chair";
+    console.log(randWordRequest);
+    getRandWordsReq(randWordRequest); 
+})
+
 submitBtn.on('click', assembleStory);
 
 //function that handles the madLibz api request
@@ -34,6 +52,21 @@ function getStory(requestUrl) {
     }).then(function(data) {
         storyData = data;
         renderInputs();
+    });
+}
+
+// function that handles the WordsAPI api request
+function getRandWordsReq(requestUrl) {
+    console.log("getRandWordsReq called");
+    fetch(requestUrl, options).then(function(response) {
+        console.log(response)
+        return response.json();
+    }).then(function(data) {
+        // console.log(data);
+        // console.log(data.results);
+        // console.log(data.results.data);
+        // console.log(data.results.data[99]);
+        console.log(data.results.data[Math.floor(Math.random() * 100)]);
     });
 }
 
@@ -48,22 +81,12 @@ function renderInputs() {
         // sets the placeholder of the input to be the needed type of the input
         blankInp.attr('placeholder', storyData.blanks[i]); 
         var randomizeBtn = $('<button>Random</button>'); // creates a new button element with the text "Random"
+        // console.log(randomizeBtn);
+        randomizeBtn.addClass("random");
         //appends the generated elements to the page
         inputForm.append(row);
         row.append(blankInp);
         row.append(randomizeBtn);
-        randomizeBtn.on('click', function() {
-            var formChildren;
-            event.preventDefault();
-            console.log("Random button is clicked");
-            var randWordRequest = wordRequest + "?partOfSpeech=" + blankInp.attr('placeholder');
-            console.log(randWordRequest);
-            getRandWordsReq(randWordRequest);
-        })
-        // randomizeBtn.on('click', function() {
-      
-        // });
-
     }
 
     //appends a submit button to the buttom of the form
@@ -80,37 +103,6 @@ function assembleStory(event) {
 
 }
 
-// function that handles the WordsAPI api request
-function getRandWordsReq(requestUrl) {
-    fetch(requestUrl).then(function(response) {
-        return response.json();
-    }).then(function(data) {
-        console.log(data);
-        return 
-    });
-}
-
-// Need to delegate event listener 
-
-// function randomizeWord () {
-
-//     // if placeholder requires this type of word
-//     // grab the placeholder value and search a random word based on that
-//     // grabbing verb
-//     // https://wordsapiv1.p.mashape.com/words/?partOfSpeech=verb
-// } 
-
-
-    // var storyEl = $('<p>'); // makes a new <p> element
-    
-    // //loops through the data object's arrays to concatenate the story
-    // //into a single paragraph and assigns that to the <p> element
-    // storyEl.text(storyData.value[0]);
-    // for (var i = 1; i < storyData.value.length-1; i++) {
-    //     var input = inputForm.children().eq(i).children('input').val();
-    //     var nextLine = storyData.value[i];
-    //     storyEl.text(storyEl.text()+input+nextLine);
-    // }
     // //appends the <p> element to the page
     // $('body').append(storyEl);
     var storyEl = $('<p>'); // makes a new <p> element
@@ -128,7 +120,6 @@ function getRandWordsReq(requestUrl) {
     $('.divBox').append(resetBtn);
     //calls a function to save the completed story into localStorage
     saveStory(storyEl.text());
-}
 
 //saves the story to localStorage
 function saveStory(content) {
